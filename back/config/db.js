9be97512ab4089aa9e3c2359b5d05f5e0196535c
Isaac Njenga <ayzzoh20@gmail.com>
2025-10-08 12:17:23 +0300

    fixed string

--- back/config/db.js
+++ back/config/db.js
@@ -1,26 +1,43 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-let isConnected = false; // Global connection flag
-
-export const connectDB = async () => {
-  if (isConnected) {
-    // console.log("🟢 Using existing database connection");
-    return;
-  }
-
+const Connection = async () => {
   try {
     const db = process.env.URI;
-    const conn = await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = conn.connections[0].readyState === 1;
-    console.log("✅ Database Connected");
+    await mongoose.connect(db);
+    console.log("Database Connected");
   } catch (error) {
-    console.error("❌ Database connection error:", error);
+    console.error("Error connecting to db", error);
   }
 };
+
+Connection();
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// let isConnected = false; // Global connection flag
+
+// export const connectDB = async () => {
+//   if (isConnected) {
+//     // console.log("🟢 Using existing database connection");
+//     return;
+//   }
+
+//   try {
+//     const db = process.env.URI;
+//     const conn = await mongoose.connect(db, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     isConnected = conn.connections[0].readyState === 1;
+//     console.log("✅ Database Connected");
+//   } catch (error) {
+//     console.error("❌ Database connection error:", error);
+//   }
+// };
